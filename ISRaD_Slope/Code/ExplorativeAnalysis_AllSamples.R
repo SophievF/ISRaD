7ba88ab525ea612e991bb86f4169b9cee4dd97cb
SophievF <sfromm@bgc-jena.mpg.de>
2022-09-29 17:29:07 +0200

    updated code

---
+++ ISRaD_Slope/Code/ExplorativeAnalysis_AllSamples.R
@@ -0,0 +1,93 @@
+# Explore 14C profiles in ISRaD #
+# Relationship between 14C and depth/SOC: all profiles #
+# Sophie von Fromm #
+# 15/09/2022 #
+
+library(tidyverse)
+library(ggpubr)
+library(mpspline2)
+
+#Load filtered lyr data
+lyr_all <- readRDS(paste0(getwd(), "/Data/ISRaD_lyr_data_filtered_2022-09-22"))
+
+lyr_all %>% 
+  count(entry_name)
+
+names(lyr_all)
+
+lyr_data <- lyr_all %>% 
+  filter(lyr_obs_date_y > 1959) %>% 
+  mutate(ClimateZone = case_when(
+    pro_usda_soil_order == "Andisols" ~ "andisols",
+    str_detect(pro_KG_present_long, "Tropical") ~ "tropical",
+    str_detect(pro_KG_present_long, "Temperate") ~ "temperate",
+    str_detect(pro_KG_present_long, "Cold") ~ "cold",
+    str_detect(pro_KG_present_long, "Polar") ~ "polar",
+    str_detect(pro_KG_present_long, "Arid") ~ "arid",
+  )) 
+
+### Density distribution
+p1 <- lyr_data %>% 
+  ggplot(aes(x = CORG, y = lyr_14c)) + 
+  geom_hex(binwidth = c(0.1,50)) +
+  theme_bw(base_size = 16) +
+  theme(axis.text = element_text(color = "black")) +
+  scale_x_continuous("SOC [wt-%]", trans = "log10", expand = c(0,0)) +
+  scale_y_continuous(expression(paste(Delta^14, "C [‰]")), expand = c(0,0),
+                     limits = c(-1005,400)) +
+  scale_fill_viridis_c(trans = "log10", direction = -1, limits = c(1,340)) +
+  coord_cartesian(ylim = c(-1000,400))
+
+p2 <- lyr_data %>% 
+  ggplot(aes(x = depth, y = lyr_14c)) + 
+  geom_hex(binwidth = c(10,50)) +
+  theme_bw(base_size = 16) +
+  theme(axis.text = element_text(color = "black")) +
+  scale_x_continuous("Depth [cm]", expand = c(0,0), limits = c(-5,510)) +
+  scale_y_continuous(expression(paste(Delta^14, "C [‰]")), expand = c(0,0),
+                     limits = c(-1000,400)) +
+  scale_fill_viridis_c(direction = -1, trans = "log10", limits = c(1,340)) +
+  coord_cartesian(xlim = c(0,500))
+
+ggarrange(p1, p2, common.legend = TRUE)
+
+ggsave(file = paste0("./Figure/ISRaD_14C_SOC_depth_hex_", Sys.Date(),
+                     ".jpeg"), width = 12, height = 6)
+
+lyr_data %>% 
+  # filter(lyr_obs_date_y > 1959) %>% 
+  # group_by(id) %>%
+  # #Filter for studies that have more than 2 depth layers
+  # filter(n() > 2) %>%
+  # arrange(depth, .by_group = TRUE) %>% 
+  # ungroup() %>% 
+  # filter(entry_name != "Fernandez_1993a") %>% 
+  ggplot(aes(x = CORG, y = lyr_14c)) + 
+  geom_hex(binwidth = c(0.1,50)) +
+  facet_wrap(~ClimateZone) +
+  theme_bw(base_size = 16) +
+  theme(axis.text = element_text(color = "black")) +
+  scale_x_continuous("SOC [wt-%]", trans = "log10") +
+  scale_y_continuous(expression(paste(Delta^14, "C [‰]")), expand = c(0,0),
+                     limits = c(-1005,400)) +
+  scale_fill_viridis_c(direction = -1) +
+  coord_cartesian(ylim = c(-1000,400)) 
+
+ggsave(file = paste0("./Figure/ISRaD_14C_SOC_climate_hex_", Sys.Date(),
+                     ".jpeg"), width = 12, height = 6)
+
+lyr_data %>% 
+  ggplot(aes(x = depth, y = lyr_14c)) + 
+  geom_hex(binwidth = c(10,50)) +
+  facet_wrap(~ClimateZone) +
+  theme_bw(base_size = 16) +
+  theme(axis.text = element_text(color = "black")) +
+  scale_x_continuous("Depth [cm]", expand = c(0,0), limits = c(-5,510)) +
+  scale_y_continuous(expression(paste(Delta^14, "C [‰]")), expand = c(0,0),
+                     limits = c(-1300,400)) +
+  scale_fill_viridis_c(direction = -1, trans = "log10", limits = c(1,340)) +
+  coord_cartesian(xlim = c(0,500), ylim = c(-1000,400)) 
+
+ggsave(file = paste0("./Figure/ISRaD_14C_depth_climate_hex_", Sys.Date(),
+                     ".jpeg"), width = 12, height = 6)
+
